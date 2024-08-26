3f66bd9e04b2fc55f233bed989a1c98644eea480
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-08-27 00:07:13 +0200

    feat: add tmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { client } from 'mtmi'
+
 function App() {
+  client.connect({ channels: ['afor_digital'] })
+
+  client.on('message', ({ username, channel, message }) => {
+    console.log(`${channel} [${username}]: ${message}`)
+  })
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[#050505]">
       <p>Suscríbete</p>
