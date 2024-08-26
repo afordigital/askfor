c04ee3d1177bcbdf9dfbdedd0a64d60d31a527f2
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2024-08-27 00:03:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-[#050505]">
+      <p>Suscríbete</p>
+    </div>
+  )
+}
+
+export default App
